9345fd2b8599e4eef1b2ab7bc2fd83809686afc1
tratchapong <tratchapong@gmail.com>
2023-06-22 01:34:00 +0700

    socket-io : start connect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
-function App() {
+import {useState, useEffect} from 'react'
+import {socket} from './socket'
 
+function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected)
+
+  useEffect( ()=> {
+    socket.on('connect', ()=> {
+      setIsConnected(true)
+    })
+    socket.on('disconnect', ()=> {
+      setIsConnected(false)
+    })
+    console.log(socket.connected)
+
+    return ()=> {
+      // socket.off('connect', ()=> {
+      //   setIsConnected(true)
+      // })
+      // socket.off('disconnect', ()=> {
+      //   setIsConnected(false)
+      // })
+    }
+  },[])
+
+  // const hdlClick = () => {
+  //   socket.connect()
+  // }
   return (
-    <>
+    <div className='max-w-6xl mx-auto grid grid-cols-1 gap-3 w-3/4'>
       <div className="text-2xl">App</div>
-      <button className="btn btn-primary">OK</button>
-    </>
+      <div className="text-2xl">Connected : {`${isConnected}`}</div>
+      <button className="btn btn-primary" onClick={()=>socket.connect()}>Connect</button>
+      <button className="btn btn-primary" onClick={()=>socket.disconnect()}>Disconnect</button>
+      <div className="divider"></div>
+      
+    </div>
   )
 }
 
