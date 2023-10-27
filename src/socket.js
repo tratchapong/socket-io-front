d58aa49c88e1f6f25bbdf5bd2636ed4d3ab6a92b
tratchapong <tratchapong@gmail.com>
2023-10-27 16:38:37 +0700

    replace URL in socket.js for connect backend at onrender.com

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,7 @@
 import {io} from 'socket.io-client'
 
-const URL = 'http://localhost:8080'
+// const URL = 'http://localhost:8080'
+const URL = 'https://socket-io-back-pteh.onrender.com'
 
 export const socket = io(URL, {
   autoConnect : false,
